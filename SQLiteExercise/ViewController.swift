68e556a45e9583776148b7b2a9c30df2c2c2cc00
camilo cabana <camilo_11_04@hotmail.com>
2019-10-07 16:35:12 -0500

    list

--- SQLiteExercise/ViewController.swift
+++ SQLiteExercise/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
+var hero = [String]()
+
 class ViewController: UIViewController {
+    
+    var dbPointer: OpaquePointer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        var dbPointer: OpaquePointer?
+        
         do {
             let fileUrl = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("HeroDatabase.sqlite")
             
@@ -29,34 +33,77 @@ class ViewController: UIViewController {
                 return
             }
             
-            var statement: OpaquePointer?
-            let insertStatement = "INSERT INTO Heroes(name) VALUES ('Minami Kikuchi') "
+//            var statement: OpaquePointer?
+//            let insertStatement = "INSERT INTO Heroes(name) VALUES ('Minami Kikuchi') "
+//
+//            sqlite3_prepare_v2(dbPointer, insertStatement, -1, &statement, nil)
+//
+//            if sqlite3_step(statement) != SQLITE_DONE {
+//                print("error inserted element!!")
+//            }
+//            sqlite3_finalize(statement)
             
-            sqlite3_prepare_v2(dbPointer, insertStatement, -1, &statement, nil)
+//            var selectStatement: OpaquePointer?
+//            let selectSql = "select * from Heroes"
+//            if sqlite3_prepare_v2(dbPointer, selectSql, -1, &selectStatement, nil) == SQLITE_OK {
+//                while sqlite3_step(selectStatement) == SQLITE_ROW {
+//                    let rowId = sqlite3_column_int(selectStatement, 0)
+//                    let name = UnsafePointer<cc_t>(sqlite3_column_text(selectStatement, 1))
+//                    let name = sqlite3_column_text(selectStatement, 1)
+//                    let stringName = String(cString: name!)
+//                    print("\(rowId): \(stringName)")
+//                }
+//            }
+//            sqlite3_finalize(selectStatement)
+        
+        } catch { }
+        
+        
+    }
+    
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add", message: "Please Add a Hero", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Add a name"
+            textField.textAlignment = .right
+        }
+        let actionOne = UIAlertAction(title: "Add", style: .default) { (actionOne) in
+            let heroName = alert.textFields?[0].text
+            var statement: OpaquePointer?
+            let insertStatement = "INSERT INTO Heroes(name) VALUES ('\(heroName!)')"
+            sqlite3_prepare_v2(self.dbPointer, insertStatement, -1, &statement, nil)
             
             if sqlite3_step(statement) != SQLITE_DONE {
                 print("error inserted element!!")
             }
             sqlite3_finalize(statement)
             
-            var selectStatement: OpaquePointer?
-            let selectSql = "select * from Heroes"
-            if sqlite3_prepare_v2(dbPointer, selectSql, -1, &selectStatement, nil) == SQLITE_OK {
-                while sqlite3_step(selectStatement) == SQLITE_ROW {
-                    let rowId = sqlite3_column_int(selectStatement, 0)
-//                    let name = UnsafePointer<cc_t>(sqlite3_column_text(selectStatement, 1))
-                    let name = sqlite3_column_text(selectStatement, 1)
-                    let stringName = String(cString: name!)
-                    print("\(rowId): \(stringName)")
-                }
-            }
-            sqlite3_finalize(selectStatement)
-        
-        } catch { }
-        
+        }
+        let actionTwo = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(actionOne)
+        alert.addAction(actionTwo)
+        present(alert, animated: true, completion: nil)
         
     }
-
+    
+    
+    @IBAction func listButton(_ sender: Any) {
+        var selectStatement: OpaquePointer?
+        let selectSql = "select * from Heroes"
+//        hero = [String]()
+        if sqlite3_prepare_v2(dbPointer, selectSql, -1, &selectStatement, nil) == SQLITE_OK {
+            while sqlite3_step(selectStatement) == SQLITE_ROW {
+                let rowId = sqlite3_column_int(selectStatement, 0)
+                let name = sqlite3_column_text(selectStatement, 1)
+                let stringName = String(cString: name!)
+                print("\(rowId): \(stringName)")
+//                hero.append(stringName)
+            }
+        }
+        sqlite3_finalize(selectStatement)
+    }
+    
 
 }
 
